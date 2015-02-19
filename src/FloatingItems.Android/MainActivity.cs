00f102a7f29b9f5b27d238e8b40588f68d866eab
wislon <wislon@gmail.com>
2015-02-19 11:13:09 +1000

    update MainActivity to to set up ccs application/applicationdelegate etc.

--- src/FloatingItems.Android/MainActivity.cs
+++ src/FloatingItems.Android/MainActivity.cs
@@ -5,27 +5,30 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using CocosSharp;
 
 namespace FloatingItems.Android
 {
     [Activity(Label = "FloatingItems.Android", MainLauncher = true, Icon = "@drawable/icon")]
     public class MainActivity : Activity
     {
-        int count = 1;
 
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
 
-            // Set our view from the "main" layout resource
-            SetContentView(Resource.Layout.Main);
+            var application = new CCApplication();
 
-            // Get our button from the layout resource,
-            // and attach an event to it
-            Button button = FindViewById<Button>(Resource.Id.MyButton);
 
-            button.Click += delegate { button.Text = string.Format("{0} clicks!", count++); };
+            application.ApplicationDelegate = new GameAppDelegate();
+            SetContentView(application.AndroidContentView);
+            application.StartGame();
+
         }
+    }
+
+    public class GameAppDelegate : CCApplicationDelegate
+    {
     }
 }
 
