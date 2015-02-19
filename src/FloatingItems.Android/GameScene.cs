4e4879f3e538bf746ca390a782188de5c22918bc
wislon <wislon@gmail.com>
2015-02-19 11:52:43 +1000

    set up initial state, get screen edges, random velocity, etc.

--- src/FloatingItems.Android/GameScene.cs
+++ src/FloatingItems.Android/GameScene.cs
@@ -1,19 +1,49 @@
 ﻿using CocosSharp;
+using Java.Lang;
 
 namespace FloatingItems.Android
 {
     public class GameScene : CCScene
     {
-        private CCLayer mainLayer;
-        private CCSprite headphoneSprite;
+        private float xVelocity;
+        private float yVelocity;
+        private readonly CCSprite headphoneSprite;
+        private float screenRight;
+        private float screenLeft;
+        private float screenTop;
+        private float screenBottom;
+
+        const float MinXVelocity = -300;
+        const float MaxXVelocity = 300;
+        const float MinYVelocity = - 300;
+        const float MaxYVelocity = 300;
 
         public GameScene(CCWindow window) : base(window)
         {
-            mainLayer = new CCLayer();
+            var mainLayer = new CCLayer();
             AddChild(mainLayer);
 
             headphoneSprite = new CCSprite("red_beats") {PositionX = 100, PositionY = 100, Scale = 0.5f};
             mainLayer.AddChild(headphoneSprite);
+
+            xVelocity = CCRandom.GetRandomFloat(MinXVelocity, MaxXVelocity);
+            yVelocity = CCRandom.GetRandomFloat(MinYVelocity, MaxYVelocity);
+
+            // get screen edges
+            screenRight = mainLayer.VisibleBoundsWorldspace.MaxX;
+            screenLeft = mainLayer.VisibleBoundsWorldspace.MinX;
+            screenTop = mainLayer.VisibleBoundsWorldspace.MaxY;
+            screenBottom = mainLayer.VisibleBoundsWorldspace.MinY;
+
+
+            Schedule(RunFloatingLogic);
+        }
+
+        private void RunFloatingLogic(float frameTimeInSeconds)
+        {
+            float currentX = xVelocity * frameTimeInSeconds;
+            float currentY = yVelocity * frameTimeInSeconds;
+            headphoneSprite.Position = new CCPoint(currentX, currentY);
         }
     }
 }
