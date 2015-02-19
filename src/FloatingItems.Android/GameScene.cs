28f99c14f8a65ed6d8ed270db4a9132b26cb46ea
wislon <wislon@gmail.com>
2015-02-19 11:41:12 +1000

    add headphone sprite, scale to 50% and put it at 100,100px

--- src/FloatingItems.Android/GameScene.cs
+++ src/FloatingItems.Android/GameScene.cs
@@ -5,11 +5,15 @@ namespace FloatingItems.Android
     public class GameScene : CCScene
     {
         private CCLayer mainLayer;
+        private CCSprite headphoneSprite;
 
         public GameScene(CCWindow window) : base(window)
         {
             mainLayer = new CCLayer();
             AddChild(mainLayer);
+
+            headphoneSprite = new CCSprite("red_beats") {PositionX = 100, PositionY = 100, Scale = 0.5f};
+            mainLayer.AddChild(headphoneSprite);
         }
     }
 }
