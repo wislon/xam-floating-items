88dd8879b1aa1ec45b6cab35e21745c3e76bf7fd
wislon <wislon@gmail.com>
2015-02-19 11:31:24 +1000

    add initial 'GameScene' base layer

---
+++ src/FloatingItems.Android/GameScene.cs
@@ -0,0 +1,15 @@
+﻿using CocosSharp;
+
+namespace FloatingItems.Android
+{
+    public class GameScene : CCScene
+    {
+        private CCLayer mainLayer;
+
+        public GameScene(CCWindow window) : base(window)
+        {
+            mainLayer = new CCLayer();
+            AddChild(mainLayer);
+        }
+    }
+}
